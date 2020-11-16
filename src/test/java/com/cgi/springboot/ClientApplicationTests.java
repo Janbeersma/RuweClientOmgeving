2e44d04465233f43486d12f40184a23e8a332a6b
florian.romijn <florian.romijn@cgi.com>
2020-11-16 14:22:55 +0100

    Clean springboot app

---
+++ src/test/java/com/cgi/springboot/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgi.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
